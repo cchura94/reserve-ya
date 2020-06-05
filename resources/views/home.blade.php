5cc1de37054a718fca7074b573cb89c216fe5597
cchura94 <cchura.cpc@gmail.com>
2020-06-05 16:58:45 -0400

    modificado 1 por cristian desde ramaCris

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Modificado por cristian ramaCris
                 </div>
             </div>
         </div>
